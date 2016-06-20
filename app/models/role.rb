facc4ebd109f4096671d308ed4c922c61dfb1470
Alexandre Duquenne <duquenne.alexandre22@gmail.com>
2016-06-20 16:10:34 +0200

    roles id

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,6 +3,7 @@ class Role < ActiveRecord::Base
   belongs_to :project
 
   def self.roles_titles
-      ['Acteur 1er rôle', 'Acteur 2nd rôle', 'Figurant', 'Cadreur', 'Directeur de la Photographie', 'Monteur', 'Ingénieur Son', 'Chef Opérateur'].sort!
+      ['Acteur 1er rôle', 'Acteur 2nd rôle', 'Figurant', 'Cadreur', 'Directeur de la Photographie', 'Monteur', 'Ingénieur Son', 'Chef Opérateur']
+  
   end
 end
