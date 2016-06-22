cd78d3e5cb15e3ec55c47b8875a7195719dfb686
Leo Vannier <leovannier@sent.com>
2016-06-22 19:25:25 +0200

    hello

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,6 +3,6 @@ class Role < ActiveRecord::Base
   belongs_to :project
 
   def self.roles_titles
-    ['Actor (leading role)', 'Actor (supporting role)', 'Camera Operator', 'Director', 'Extra', 'Make Up & Costumes', 'Photographer', 'Script Supervisor', 'Sound Director', 'Video Editor']
+    ['Actor (leading role)', 'Actor (supporting role)', 'Camera Operator', 'Director', 'Extra', 'Illustrator', 'Make Up & Costumes', 'Photographer', 'Script Supervisor', 'Sound Director', 'Video Editor', 'Voice Actor']
   end
 end
